7d68edc22d1baaaa2a11df25222607f121a0b2ef
Rodrigo S. Jauregui <rodrigo.jauregui@correo.ucu.edu.uy>
2024-09-01 01:05:29 -0300

    Correciones al manejo de los datos de las clases.

--- app/src/main/java/com/example/ucu2024_android_lazylayouts_leccion1/models/Conversacion.kt
+++ app/src/main/java/com/example/ucu2024_android_lazylayouts_leccion1/models/Conversacion.kt
@@ -1,7 +1,67 @@
 package com.example.ucu2024_android_lazylayouts_leccion1.models
 
-class Conversacion(
-    val Contacto: Contacto,
-) {
-    val Mensajes: MutableList<Mensaje> = mutableListOf()
+import androidx.compose.ui.util.fastMap
+import java.time.Instant
+import java.util.Date
+import java.util.UUID
+
+class Conversacion private constructor(miembros: MutableList<Contacto>) {
+    private val _mensajes: MutableList<Mensaje> = mutableListOf()
+    private var _tipo: TiposConversacion? = null
+    private val _contactos: MutableMap<UUID, Contacto> = mutableMapOf()
+    private val _lecturas: MutableMap<UUID, Date> = mutableMapOf()
+
+    val Contactos: MutableList<Contacto>
+        get() = mutableListOf( *_contactos.map { it.value.Clonar() }.toTypedArray() )
+
+
+    init {
+        miembros.forEach { _lecturas[it.Id] = Date.from(Instant.now()) }
+    }
+
+    constructor(first: Contacto, second: Contacto) : this(mutableListOf(first, second)) {
+        _getNombre = { consultor: UUID ->
+            _contactos.filter { it.key != consultor }.values.randomOrNull()?.Nombre?:"Desconocido"
+        }
+        _tipo = TiposConversacion.Privado
+    }
+
+    constructor(miembros: MutableList<Contacto>, nombre: String) : this(miembros) {
+        if (miembros.count() < 2)
+            throw IllegalArgumentException("Grupo debe contener 2 o mas miembros.")
+
+        _getNombre = { consultor: UUID -> nombre }
+        _tipo = TiposConversacion.Grupal
+    }
+
+
+    fun GetNombre(consultor: UUID): String {
+        return _getNombre(consultor)
+    }
+
+    private var _getNombre: (consultor: UUID) -> String = {
+        throw UninitializedPropertyAccessException("Grupo no inicializado.")
+    }
+
+    fun EnviarMensaje(mensaje: Mensaje): Unit {
+        _mensajes.add(mensaje)
+        _lecturas[mensaje.Emisor] = Date.from(Instant.now())
+    }
+
+    fun GetMensajes(consultor: UUID): MutableMap<Mensaje, Boolean> {
+        _lecturas[consultor] = Date.from(Instant.now())
+        return mutableMapOf( *_mensajes.fastMap { it.Clonar() to (it.Enviado > _lecturas[it.Emisor]) }.toTypedArray() )
+    }
+
+    fun GetUltimoMensaje(): Mensaje {
+        return _mensajes.last().Clonar()
+    }
+
+    fun GetMensajesNuevos(consultor: UUID): Int {
+        return _mensajes.filter { it.Enviado > _lecturas[consultor] }.count()
+    }
+}
+
+enum class TiposConversacion {
+    Privado, Grupal
 }
--- app/src/main/java/com/example/ucu2024_android_lazylayouts_leccion1/models/Mensaje.kt
+++ app/src/main/java/com/example/ucu2024_android_lazylayouts_leccion1/models/Mensaje.kt
@@ -2,30 +2,53 @@ package com.example.ucu2024_android_lazylayouts_leccion1.models
 
 import java.time.Instant
 import java.util.Date
+import java.util.UUID
 
 class Mensaje(
     contenido: String,
+    val Emisor: UUID,
     val Enviado: Date,
 ) {
     var Contenido: String = contenido
-        set(value) {
-            if (!Eliminado) {
-                field = value
-                Modificado = Date.from(Instant.now())
-            }
-            else throw IllegalStateException("El mensaje ha sido eliminado.")
-        }
+        private set
 
     var Modificado: Date? = null
-        private set(value) { field = value }
+        private set
 
     var Eliminado: Boolean = false
-        private set(value) {
-            field = value
+        private set
+
+
+    constructor(contenido: String, emisor: UUID) : this(
+        contenido = contenido,
+        Emisor = emisor,
+        Enviado = Date.from(Instant.now())
+    )
+
+    private constructor(original: Mensaje) : this(
+        contenido = original.Contenido,
+        Emisor = original.Emisor,
+        Enviado = original.Enviado
+    ) {
+        Modificado = original.Modificado
+        Eliminado = original.Eliminado
+    }
+
+
+    fun Editar(contenido: String) {
+        if (!Eliminado) {
+            Contenido = contenido
             Modificado = Date.from(Instant.now())
         }
+        else throw IllegalStateException("El mensaje ha sido eliminado.")
+    }
 
-    constructor(contenido: String) : this(contenido, Date.from(Instant.now()))
+    fun ToggleEliminado() {
+        Eliminado = !Eliminado
+        Modificado = Date.from(Instant.now())
+    }
 
-    val ToggleEliminado: () -> Unit = { Eliminado = !Eliminado }
+    fun Clonar(): Mensaje {
+        return Mensaje(original = this)
+    }
 }
